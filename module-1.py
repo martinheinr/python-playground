7b194cd96b4b912ec9b5f8bfc011e02a3150c228
Martin <martin.heinrich91@gmail.com>
2024-01-28 19:33:09 +0100

    add files

--- module-1.py
+++
@@ -1,3 +0,0 @@
-friends = ['Jim', 'Bob', 'Tim']
-for n in friends:
-    print("Hi " + n)
---
+++ module_1.py
@@ -0,0 +1,22 @@
+def test():
+    friends = ['Jim', 'Bob', 'Tim']
+    for n in friends:
+        print("Hi " + n)
+
+test()
+
+name:str="test"
+print(name)
+
+download_size_kb = 200*1024
+total_computers = 200
+total_kbs = download_size_kb*total_computers
+
+print(total_kbs) # Should print 40960000.0
+
+bill = 47.28 # Assign "bill" variable with bill amount
+tip = bill * 0.15 # Multiply by stated tip rate 
+total = bill + tip # Sum the "total" of the "bill" and "tip"
+share = total / 2 # Divide "total" by number of friends dining
+print("Each person needs to pay: ", share) # Enter the required string and "share" 
+# Hint: Remember to convert incompatible data types
