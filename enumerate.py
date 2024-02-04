cc62e8c9ad6f7b46e9deb91db67369574cc70c74
Martin <martin.heinrich91@gmail.com>
2024-02-04 15:03:41 +0100

    Add comments

--- enumerate.py
+++ enumerate.py
@@ -1,11 +1,12 @@
 def skip_elements(elements):
     #Intialize empty list
     result = []
-    #iterate with loop through elements with enumeration function
-    #enumerate function first returns index of an element in a sequence and then the element in the sequence
+    #Iterate with loop through elements with enumeration function
+    #Enumerate function first returns index of an element in a sequence and then the element in the sequence
     for index, element in enumerate(elements):
-        #modulo condition filters to output every second index
+        #Modulo condition filters to output every second index
         if index % 2 == 0:
+            #If true, appent to the list
             result.append(element)
     return result
 print(skip_elements(["a", "b", "c", "d", "e", "f", "g"])) # Should be ['a', 'c', 'e', 'g']
