42bd23f0dbe926407b8bef774db639c7cffcc233
Martin <martin.heinrich91@gmail.com>
2024-02-04 15:01:00 +0100

    Another comprehension example

--- list_comprehension.py
+++ list_comprehension.py
@@ -7,3 +7,11 @@ print(odd_numbers(10)) # Should print [1, 3, 5, 7, 9]
 print(odd_numbers(11)) # Should print [1, 3, 5, 7, 9, 11]
 print(odd_numbers(1))  # Should print [1]
 print(odd_numbers(-1)) # Should print []
+
+
+def squares(start, end):
+    return [n*n for n in range(start, end+1)]
+
+print(squares(2, 3))    # Should print [4, 9]
+print(squares(1, 5))    # Should print [1, 4, 9, 16, 25]
+print(squares(0, 10))   # Should print [0, 1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
