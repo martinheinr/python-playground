5cf2dc0fe9165fd79426b863e3c129824d5d83fe
Martin <martin.heinrich91@gmail.com>
2024-02-09 21:33:18 +0100

    add healtcheck.py

--- interact_with_os/disk_usage.py
+++ interact_with_os/disk_usage.py
@@ -1,4 +1,15 @@
 import shutil
+
+def bytes_to_gb(bytes):
+    return bytes / (1024 * 1024 * 1024)
+
 du = shutil.disk_usage("C:")
 free_pct=du.free/du.total*100
 print("Free disk space of you r C drive is ", round(free_pct, 2), "%")
+
+dut = shutil.disk_usage("C:")
+free_dut=dut.total-dut.used
+
+print(bytes_to_gb(free_dut))
+
+
