0136bc711e99e0859e4205967ccde890ef52c1f4
Martin <martin.heinrich91@gmail.com>
2024-02-18 21:16:24 +0100

    Examples

---
+++ data_streams/arguments_and_status.py
@@ -0,0 +1,11 @@
+import sys
+import os
+#Providing a file name as a argument to this script will create a new file
+filename = sys.argv[1]
+
+if not os.path.exists(filename):
+    with open(filename, "w") as f:
+        f.write("New file created\n")
+else:
+    print("Error, the file {} already exists!".format(filename))
+    sys.exit(1)
