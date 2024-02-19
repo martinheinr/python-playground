64b5c0b9f0d22da441a512bbc8fd5ed1c4ca0db8
Martin <martin.heinrich91@gmail.com>
2024-02-19 20:34:56 +0100

    add examples subprocesses

---
+++ subprocesses/basics.py
@@ -0,0 +1,22 @@
+import subprocess
+""" subprocess.run(["date"])
+subprocess.run(["sleep", "2"])
+result = subprocess.run(["ls", "this_file_does_not_exist"])
+print(result.returncode) """
+
+result = subprocess.run(["host", "8.8.8.8"], capture_output=True)
+
+result = subprocess.run(["host", "8.8.8.8"], capture_output=True)
+print(result.returncode)
+
+result = subprocess.run(["host", "8.8.8.8"], capture_output=True)
+print(result.stdout)
+
+result = subprocess.run(["host", "8.8.8.8"], capture_output=True)
+print(result.stdout.decode().split())
+
+
+result = subprocess.run(["rm", "does_not_exist"], capture_output=True)
+print(result.returncode)
+print(result.stdout)
+print(result.stderr)
