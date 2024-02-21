8321db7cb702b220df78cad6ca3a990a361fe148
Martin <martin.heinrich91@gmail.com>
2024-02-21 20:02:29 +0100

    FIx test

--- testing/pytest_examples.py
+++ testing/pytest_examples.py
@@ -2,3 +2,12 @@ def divide(a, b):
 	assert b != 0, "Cannot divide by zero"
 	return a / b
 print(divide(0, 4))
+
+import re
+def rearrange_name(name):
+ result = re.search(r"^([\w .]*), ([\w .]*)$", name)
+ return "{} {}".format(result[2], result[1])
+from rearrange import rearrange_name
+
+
+rearrange_name("Lovelace, Ada")  
--- testing/pytest_fixtures.py
+++ testing/pytest_fixtures.py
@@ -20,14 +20,14 @@ class FruitSalad:
         for fruit in self.fruit:
             fruit.cube()
 
-    # Arrange
-    @pytest.fixture
-    def fruit_bowl():
-        return [Fruit("apple"), Fruit("banana")]
+# Arrange
+@pytest.fixture
+def fruit_bowl():
+    return [Fruit("apple"), Fruit("banana")]
 
 
-    def test_fruit_salad(fruit_bowl):
-    # Act
-        fruit_salad = FruitSalad(*fruit_bowl)
-    # Assert
+def test_fruit_salad(fruit_bowl):
+# Act
+    fruit_salad = FruitSalad(*fruit_bowl)
+# Assert
     assert all(fruit.cubed for fruit in fruit_salad.fruit)
