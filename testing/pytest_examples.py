9f17b624c0e9487730c3ddf9cf5b905bccf08466
Martin <martin.heinrich91@gmail.com>
2024-02-22 20:33:45 +0100

    Add example of an unit test

--- testing/pytest_examples.py
+++ testing/pytest_examples.py
@@ -4,10 +4,12 @@ def divide(a, b):
 print(divide(0, 4))
 
 import re
+from rearrange import rearrange_name
+
 def rearrange_name(name):
  result = re.search(r"^([\w .]*), ([\w .]*)$", name)
  return "{} {}".format(result[2], result[1])
-from rearrange import rearrange_name
 
 
-rearrange_name("Lovelace, Ada")  
+rearranged_name = rearrange_name("Lovelace, Ada")
+print(rearranged_name)
