718271d521e0d481c612113600da85cb01114cc6
Martin <martin.heinrich91@gmail.com>
2024-02-23 20:59:00 +0100

    Add more test cases to rearrange_test.py

--- testing/rearrange_test.py
+++ testing/rearrange_test.py
@@ -1,3 +1,10 @@
+""" The building blocks of unit tests within the unittest module are test cases, which enable developers to run multiple tests at once. 
+To write test cases, developers need to write subclasses of TestCase or use FunctionTestCase. 
+
+To perform a specific test, the TestCase subclass needs to implement a test method that starts with the name test. 
+This identifier is what informs the test runner about which methods represent tests.
+ """
+
 from rearrange import rearrange_name
 import unittest
 
@@ -5,6 +12,16 @@ class TestRearrange(unittest.TestCase):
   def test_basic(self):
     testcase = "Lovelace, Ada"
     expected = "Ada Lovelace"
+    self.assertEqual(rearrange_name(testcase), expected)
+
+  def test_one_name(self):
+    testcase = "Voltaire"
+    expected = "Voltaire"
+    self.assertEqual(rearrange_name(testcase), expected)
+    
+  def test_double_name(self):
+    testcase = "Hopper, Grace M."
+    expected = "Grace M. Hopper"
     self.assertEqual(rearrange_name(testcase), expected)
 
   def test__numbers(self):
