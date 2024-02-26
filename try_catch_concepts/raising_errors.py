78be51d563cf4a426a74afe94325d07e981c02fe
Martin <martin.heinrich91@gmail.com>
2024-02-26 19:56:09 +0100

    Adding unittests for validate_user

---
+++ try_catch_concepts/raising_errors.py
@@ -0,0 +1,26 @@
+def validate_user(username, minlen):
+  if minlen < 1:
+    raise ValueError("minlen must be at least 1")
+
+  if len(username) < minlen:
+    return False
+  if not username.isalnum():
+    return False
+  return True
+
+#string_1 = validate_user("a", 0)
+
+
+def validate_username(username, minlen):
+  assert type(username) == str, "username must be a string"
+  if minlen < 1:
+    raise ValueError("minlen must be at least 1")
+
+  if len(username) < minlen:
+    return False
+  if not username.isalnum():
+    return False
+  return True
+
+
+
