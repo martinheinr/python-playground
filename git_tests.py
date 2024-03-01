f5727982808671faa85b8c6b49758cfcfb99bd0f
Martin <martin.heinrich91@gmail.com>
2024-03-01 21:29:47 +0100

    Replaced string in git_tests.py

--- git_tests.py
+++ git_tests.py
@@ -1,4 +1,15 @@
+import sys
+import os
+
+def check_reboot():
+    """Returns True if the computer has a pending reboot."""
+    return os.path.exists("/run/reboot-required")
+
 def main():
-    pass
+    if check_reboot():
+        print("Pending Reboot.")
+        sys.exit(1)
+    else:
+        print("No reboot pending")
 
 main()
